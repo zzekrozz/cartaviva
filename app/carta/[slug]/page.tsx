f5b5c590e8d26b9a764c98d2d0fa377f29d0fec5
zzekrozz <pogrebnyakivan123@gmail.com>
2026-05-25 18:17:29 +0200

    Delete app directory

--- app/carta/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ClientCartaPage from "./ClientCartaPage";
-
-export default function CartaPage({ params }: { params: { slug: string } }) {
-  return <ClientCartaPage slug={params.slug} />;
-}
