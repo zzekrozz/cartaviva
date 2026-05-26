d7acc840cff4b808a30490cee8a360b6bc7975f7
zzekrozz <pogrebnyakivan123@gmail.com>
2026-05-26 19:21:15 +0200

    Delete lib directory

--- lib/brand.ts
+++
@@ -1,3 +0,0 @@
-export const BRAND_NAME = "MesaCarta";
-export const BRAND_TAGLINE = "Miniweb visual para restaurantes con carta digital y menu del dia.";
-export const BRAND_DOMAIN_PLACEHOLDER = "mesacarta.app";
